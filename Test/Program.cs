610e7be841b7658222e4bc982ccc35a6c236c73a
Анатолий <92164506+AnatolyED@users.noreply.github.com>
2024-03-23 02:17:42 +1000

    Final
    Закончил

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,71 +1,117 @@
-﻿namespace Test
+using System;
+
+namespace Test
 {
     public class Program
     {
-        private static float result = 0;
-        private static List<float> numbers;
-        private static bool accesNextProccesCircle = true;
+        private static float _result = 0;
+        private static int _countNumber = 0;
+        private static List<float> _numbers;
+        private static bool _acceptOperation = true;
         private static void Main(string[] args)
         {
-            bool exit = false;
-            while (!exit)
+            Console.WriteLine("Приветствую в калькуляторе!\nВведите кол-во чисел:");
+            int.TryParse(Console.ReadLine(),out _countNumber);
+
+            if (_countNumber > 0)
+                WriteNumbers();
+            else
             {
-                List<float> numbers;
-
-                Console.WriteLine("Приветствую в калькуляторе!\nВведите кол-во чисел:");
-                int countNumber = Convert.ToInt32(Console.ReadLine());
-
-                numbers = new List<float>(countNumber);
-                WriteNumbers(numbers);
+                Console.WriteLine("Должно быть хотя-бы 1 число!");
+                _acceptOperation = false;
+            }
+            if (_acceptOperation)
+            {
+                Console.WriteLine("Выберите одну из операций: | + | - | * | / |");
                 SelectOperation(Console.ReadLine());
             }
+            Console.WriteLine("Нажмите любую клавишу чтобы выйти.");
+            Console.ReadKey();
         }
 
         private static void SelectOperation(string symbol)
         {
-            Console.WriteLine("+ / - / * / :");
+            bool acceptOperation = true;
             switch (symbol)
             {
                 case "+":
-                    foreach (var item in numbers)
+                    foreach (var item in _numbers)
                     {
-                        result += item;
+                        _result += item;
                     }
-                    Console.WriteLine(result);
                     break;
                 case "-":
-                    foreach (var item in numbers)
+                    foreach (var item in _numbers)
                     {
-                        result -= item;
+                        _result -= item;
                     }
-                    Console.WriteLine(result);
                     break;
                 case "*":
-                    foreach (var item in numbers)
+                    if (_numbers.Count > 1)
                     {
-                        result *= item;
+                        _result = _numbers[0];
+                        for (int i = 1; i < _numbers.Count; i++)
+                        {
+
+                            _result *= _numbers[i];
+                        }
                     }
-                    Console.WriteLine(result);
+                    else
+                    {
+                        Console.WriteLine("Чисел меньше, чем нужно для операции. Требуется минимум 2 числа.");
+                        acceptOperation = false;
+                        break;
+                    }
                     break;
-                case ":":
-                    foreach (float item in numbers)
-                    {
-                        result /= item;
+                case "/":
+                    if (_countNumber > 1) {
+                        _result += _numbers[0];
+                        for (int i = 1; i < _numbers.Count; i++)
+                        {
+                            if (_result == 0 || _numbers[i] == 0)
+                            {
+                                Console.WriteLine("Делить на ноль нельзя.");
+                                acceptOperation = false;
+                                break;
+                            }
+                            _result /= _numbers[i];
+                        }
                     }
-                    Console.WriteLine(result);
+                    else
+                    {
+                        Console.WriteLine("Чисел меньше, чем нужно для операции. Требуется минимум 2 числа.");
+                        acceptOperation = false;
+                        break;
+                    }
                     break;
                 default:
                     Console.WriteLine("Такой операции не существует. Выберите другую операцию");
+                    acceptOperation = false;
                     break;
             }
-            result = 0;
+            if (acceptOperation)
+                Console.WriteLine(_result);
         }
 
-        private static void WriteNumbers(List<float> numbers)
+        private static void WriteNumbers()
         {
-            for(int i = 0; i < numbers.Count; i++)
+            _numbers = new List<float>(_countNumber);
+            int positionNumber;
+
+            for (int i = 0; i < _countNumber; i++)
             {
-                numbers[i] = Convert.ToSingle(Console.ReadLine());
+                positionNumber = i + 1;
+                Console.WriteLine("Введите " + positionNumber + " число");
+
+                string num = Console.ReadLine();
+                if (float.TryParse(num, out float result))
+                    _numbers.Add(result);
+                else
+                {
+                    Console.WriteLine("Ошибка данных, указано не число!");
+                    _acceptOperation = false;
+                    break;
+                }
             }
         }
     }
